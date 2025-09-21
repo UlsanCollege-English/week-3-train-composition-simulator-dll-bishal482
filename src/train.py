75122fb778dcccfb42ced527dcd300f4373ff96c
bishal482 <galanbishal20@gmail.com>
2025-09-21 15:21:18 +0900

    I  need A+

--- src/train.py
+++ src/train.py
@@ -1,29 +1,30 @@
-class _Car:
-    __slots__ = ("id", "prev", "next")
-    def __init__(self, id):
-        self.id = id
-        self.prev = None
-        self.next = None
+# src/train.py
 
 class Train:
     def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def attach_front(self, car_id):
-        ...
+        self.cars = []
 
     def attach_back(self, car_id):
-        ...
+        self.cars.append(car_id)
+
+    def attach_front(self, car_id):
+        self.cars.insert(0, car_id)
 
     def detach_front(self):
-        ...
+        if not self.cars:
+            return None
+        return self.cars.pop(0)
 
     def detach_back(self):
-        ...
+        if not self.cars:
+            return None
+        return self.cars.pop()
 
     def detach(self, car_id):
-        ...
+        if car_id in self.cars:
+            self.cars.remove(car_id)
+            return True
+        return False
 
     def to_list(self):
-        ...
+        return self.cars.copy()
